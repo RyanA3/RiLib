4f5e2bc3a8ff2221446bb3ca895bb5b692e766b7
BuildTools <unconfigured@null.spigotmc.org>
2021-01-14 16:31:08 -0500

    Reconsider Life Choices

--- RiLib/src/me/felnstaren/rilib/command/impl/TestCommand.java
+++ RiLib/src/me/felnstaren/rilib/command/impl/TestCommand.java
@@ -6,14 +6,14 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 import me.felnstaren.rilib.chat.Messenger;
-import me.felnstaren.rilib.ui.prompt.ChatPrompt;
+import me.felnstaren.rilib.ui.prompt.ChatOptionPrompt;
 import me.felnstaren.rilib.ui.prompt.PromptHandler;
 
 public class TestCommand implements CommandExecutor {
 
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 		Player player = (Player) sender;
-		ChatPrompt prompt = new ChatPrompt(player, 10, "#FD0Do you like gaming? #999[#0F0%option1#999] [#F00%option2#999]", "YES", "NO") {
+		ChatOptionPrompt prompt = new ChatOptionPrompt(player, 10, "#FD0Do you like gaming? #999[#0F0%option1#999] [#F00%option2#999]", "YES", "NO") {
 			public void callback(String response) {
 				Messenger.send(this.player, "#0F0You chose #999" + response + "#0F0!");
 				expired = true;
--- RiLib/src/me/felnstaren/rilib/ui/prompt/ChatPrompt.java
+++ RiLib/src/me/felnstaren/rilib/ui/prompt/ChatPrompt.java
@@ -2,44 +2,28 @@ package me.felnstaren.rilib.ui.prompt;
 
 import org.bukkit.entity.Player;
 
-import me.felnstaren.rilib.chat.ClickComponent;
 import me.felnstaren.rilib.chat.Message;
 import me.felnstaren.rilib.chat.Messenger;
 
 public abstract class ChatPrompt extends Prompt<String> {
-
-	protected Message message;
-	protected String[] options;
 	
-	//Formattable message format: "Something something... %#FFF|option1| %&c<option2> %#AABBCC[option3]"
-	public ChatPrompt(Player player, int time, String formattable_message, String... options) {
+	protected Message message;
+
+	public ChatPrompt(Player player, int time, String formattable_message) {
 		super(player, time);
 		this.message = Messenger.colorWithJson(formattable_message);
-		this.options = options;
-		
-		//String[] infos = new String[options.length];
-		for(int i = 1, j = 1; i < message.size(); i++) {
-			if(!message.getComponent(i).getText().startsWith("%")) continue;
-			message.getComponent(i).replaceAll("%option" + j, options[j-1]);
-			message.getComponent(i).setClickComponent(new ClickComponent("/prompt " + id + " " + options[j-1]));
-			j++;
-		}
-		
-		//for(int i = 1; i < infos.length; i++)
-		//	message.addComponent(new TextComponent(infos[i].replace("option" + i, options[i-1])).setClickComponent(new ClickComponent("/prompt " + id + " " + options[i-1])));
 	}
 
-	
-	
+
 	public void send() {
-		//RiLib.LOGGER.log(Level.DEBUG, message.build());
 		Messenger.send(player, message);
 	}
-	
+
 	public void timeout() {
 		if(expired) return;
 		expired = true;
 		Messenger.send(player, "#F22You failed to respond in time!");
 	}
 	
+
 }
