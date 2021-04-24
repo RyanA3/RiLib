a4890836bbb3b8e30e587d4eca67c76d7aff4183
BuildTools <unconfigured@null.spigotmc.org>
2021-04-24 16:54:14 -0400

    static stringutil

--- RiLib/src/me/felnstaren/felib/util/StringUtil.java
+++ RiLib/src/me/felnstaren/felib/util/StringUtil.java
@@ -2,14 +2,14 @@ package me.felnstaren.felib.util;
 
 public class StringUtil {
 
-	public boolean isAlphaNumeric(String value) {
+	public static boolean isAlphaNumeric(String value) {
 		char[] cray = value.toCharArray();
 		for(char c : cray)
 			if(!isAlphaNumeric(c)) return false;
 		return true;
 	}
 	
-	public boolean isAlphaNumeric(char value) {
+	public static boolean isAlphaNumeric(char value) {
 		return	(
 				value >= 48
 			&&	value <= 57
